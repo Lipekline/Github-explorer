4f0c0ea6a9f694f0e55653a64a5f757ec1e79d00
Luis Felipe Borges Silva <58269629+Lipekline@users.noreply.github.com>
2021-08-31 19:39:27 -0300

    adiciona landing page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { FiChevronRight } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
-}
+  return (
+  <>
+  <img src={logoImg} alt="Github Explorer" />
+  <Title>Explore repositorios no Github</Title>
+  <Form action="">
+    <input placeholder="Digite o nome do repositorio" />
+    <button type="submit">Pesquisar</button>
+  </Form>
+
+  <Repositories>
+    <a href="teste">
+    <img src="https://avatars.githubusercontent.com/u/58269629?v=4"
+    alt="Lipe Silva" />
+    <div>
+      <strong>rocketseat/unform</strong>
+      <p>Easy peasy lemon squeazy</p>
+    </div>
+
+    <FiChevronRight size={20} />
+    </a>
+    <a href="teste">
+    <img src="https://avatars.githubusercontent.com/u/58269629?v=4"
+    alt="Lipe Silva" />
+    <div>
+      <strong>rocketseat/unform</strong>
+      <p>Easy peasy lemon squeazy</p>
+    </div>
+
+    <FiChevronRight size={20} />
+    </a>
+    <a href="teste">
+    <img src="https://avatars.githubusercontent.com/u/58269629?v=4"
+    alt="Lipe Silva" />
+    <div>
+      <strong>rocketseat/unform</strong>
+      <p>Easy peasy lemon squeazy</p>
+    </div>
+
+    <FiChevronRight size={20} />
+    </a>
+  </Repositories>
+  </>
+  )
+};
 
 export default Dashboard;
